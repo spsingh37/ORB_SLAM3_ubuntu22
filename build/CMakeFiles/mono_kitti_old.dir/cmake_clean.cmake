d70bf32e7671b7a050def978d5f6a67aa8be4d4e
spsingh37 <suryasin@umich.edu>
2025-01-11 10:27:28 -0500

    added python3 support

---
+++ build/CMakeFiles/mono_kitti_old.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../Examples_old/Monocular/mono_kitti_old"
+  "../Examples_old/Monocular/mono_kitti_old.pdb"
+  "CMakeFiles/mono_kitti_old.dir/Examples_old/Monocular/mono_kitti.cc.o"
+  "CMakeFiles/mono_kitti_old.dir/Examples_old/Monocular/mono_kitti.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mono_kitti_old.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
